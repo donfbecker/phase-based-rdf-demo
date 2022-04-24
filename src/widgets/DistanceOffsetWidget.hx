e5d466bc2d8b9c8647a77b81346ace73a8c275bc
Don Becker <dbecker@ideapad.psyon.org>
2022-04-23 22:18:32 -0500

    A couple more fixes to triangulation display

--- src/widgets/DistanceOffsetWidget.hx
+++ src/widgets/DistanceOffsetWidget.hx
@@ -64,35 +64,33 @@ class DistanceOffsetWidget extends Widget {
 		var deltaPhaseSN:Float = phaseDifference(phases[0], phases[1]);
 		var deltaPhaseEW:Float = phaseDifference(phases[2], phases[3]);
 
-		// The phase difference multiplied by the wave length gives us the difference
-		// between the distances traveled to each antenna. This works when the target
-		// is further away from the antenna, but is inaccurate up close I think we need
-		// to calculate this based on hitting N first, and S first, and average them.
-		var hypotenuse:Float = waveLength * 0.48;
-		var opposite:Float = deltaPhaseNS * waveLength;
-		var asin:Float = Math.asin(opposite / hypotenuse);
-		var adjacent:Float = hypotenuse * Math.cos(asin);
-
-		// Experiment
+		// Calculate bearing based on NS pair
 		var angleNS:Float = -Math.asin(deltaPhaseNS / 0.48) * (180 / Math.PI);
 		if(deltaPhaseEW > 0) angleNS = (angleNS + 180) * -1;
 		if(angleNS < 0) angleNS += 360;
 
+		// Calculate bearing based on EW pair
 		var angleEW:Float = (Math.asin(deltaPhaseEW / 0.48) * (180 / Math.PI)) + 90;
 		if(deltaPhaseNS > 0) angleEW = angleEW * -1;
 		if(angleEW < 0) angleEW += 360;
 
+		// Average the two
 		bearing = (angleNS + angleEW) / 2;
-		trace(angleNS, angleEW, bearing);
 
 		// Scale the lines for Fronts and Deltas
+		var bearingInRadians = bearing * (Math.PI / 180);
+		var hypotenuse:Float = waveLength * 0.48;
+		var opposite:Float = hypotenuse * Math.sin(bearingInRadians);
+		var adjacent:Float = hypotenuse * Math.cos(bearingInRadians);
+		trace(opposite, adjacent);
 		var pixelsPerMeter = (spacing * 2) / hypotenuse;
+
 		frontNS.scaleX = (Math.abs(adjacent) * pixelsPerMeter) / 100;
 		deltaNS.scaleX = (Math.abs(opposite) * pixelsPerMeter) / 100;
 
 		frontNS.x = deltaNS.x = cross.x;
 		deltaNS.rotation = -bearing;
-		if(opposite < 0) {
+		if(opposite > 0) {
 			// Hit North First
 			frontNS.y = cross.y - spacing;
 			deltaNS.y = cross.y + spacing;
